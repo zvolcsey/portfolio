97e2adc3f21e98caa0ba1fda16e8ad59237c8d8c
Zoltán Völcsey <41576384+zvolcsey@users.noreply.github.com>
2025-08-29 10:11:47 +0200

    feat: add skills
    Add core tech stack to the About Me section.
    
    Update the order of the elements in the About Me section.

--- src/components/page-sections/about.tsx
+++ src/components/page-sections/about.tsx
@@ -4,14 +4,9 @@ import { Badge } from '../ui/badge';
 export default function About() {
   return (
     <section className='px-4 pt-4 pb-16 sm:pb-24'>
-      <div className={cn('mx-auto w-full max-w-3xl space-y-8', 'text-center')}>
+      <div className={cn('mx-auto w-full max-w-2xl space-y-12', 'text-center')}>
         <h2 className='text-2xl font-bold'>About Me</h2>
-        <div className='flex flex-wrap items-center justify-center gap-2'>
-          <Badge className='bg-primary'>Open Source Contributor</Badge>
-          <Badge className='border-primary'>Problem Solver</Badge>
-          <Badge className='border-primary'>TypeScript Enthusiast</Badge>
-          <Badge className='border-primary'>System-Oriented Mindset</Badge>
-        </div>
+        {/* Introduction */}
         <p className={cn('mx-auto mb-4 w-full max-w-2xl', 'text-left')}>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque
           commodi ipsa tenetur voluptas impedit. Perferendis voluptates
@@ -28,6 +23,27 @@ export default function About() {
           magnam libero voluptas eos aperiam distinctio! Ipsum odio suscipit
           quos modi ex perferendis soluta
         </p>
+        {/* Core Stack */}
+        <div className={cn('space-y-2', 'text-left')}>
+          <p className='text-sm'>Core Stack:</p>
+          <p className='flex flex-wrap items-center gap-2'>
+            <Badge className='bg-[#007acc] text-white'>TypeScript</Badge>
+            <Badge className='bg-[#f0db4f] text-black'>JavaScript (ES6+)</Badge>
+            <Badge className='bg-[#61dbfb] text-black'>React</Badge>
+            <Badge className='bg-[#000000] text-white'>Next.js</Badge>
+            <Badge className='bg-sky-500 text-black'>Tailwind CSS</Badge>
+            <Badge className='bg-[#3c873a] text-white'>Node.js</Badge>
+            <Badge className='bg-[#000000] text-white'>Express.js</Badge>
+            <Badge className='bg-[#336791] text-white'>PostgreSQL</Badge>
+          </p>
+        </div>
+        {/* Other Skills */}
+        <div className='flex flex-wrap items-center justify-end gap-2'>
+          <Badge>Open Source Contributor</Badge>
+          <Badge>Problem Solver</Badge>
+          <Badge>TypeScript Enthusiast</Badge>
+          <Badge>System-Design Fundamentals</Badge>
+        </div>
       </div>
     </section>
   );
