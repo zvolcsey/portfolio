a4a4e4091e62c7032b08b3e5df8d9162d8dbd4d2
Zoltán Völcsey <41576384+zvolcsey@users.noreply.github.com>
2025-09-11 09:43:58 +0200

    feat: implement complete layout structure
    Add flexbox to body for manage header, main, and footer.
    
    Add footer element to layout component.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,15 @@ export default function RootLayout({
     <html lang='en'>
       <body>
         <MainHeader />
-        <main className='pt-20'>{children}</main>
+        <main className='mt-20 flex-1 pb-8'>{children}</main>
+        <footer className='mt-auto border-t border-t-slate-200 py-10'>
+          <div className='container mx-auto'>
+            <p className='text-center text-sm text-slate-500'>
+              &copy; {new Date().getFullYear()} Zoltán Völcsey. All rights
+              reserved.
+            </p>
+          </div>
+        </footer>
       </body>
     </html>
   );
